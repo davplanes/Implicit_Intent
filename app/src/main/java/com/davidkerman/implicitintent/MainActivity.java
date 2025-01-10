e41895024a1f389f0c5e6c2327d057fe7714cd4c
user <davplanes@gmail.com>
2025-01-10 19:43:20 +0200

    Added menu (visuals only)

--- app/src/main/java/com/davidkerman/implicitintent/MainActivity.java
+++ app/src/main/java/com/davidkerman/implicitintent/MainActivity.java
@@ -1,14 +1,18 @@
 package com.davidkerman.implicitintent;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        InitializeViews();
+    }
+
+    private void InitializeViews() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(findViewById(R.id.toolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.msg) {
+            return true;
+        } else if (id == R.id.wa) {
+            return true;
+        } else if (id == R.id.othermsgs) {
+            return true;
+        } else if (id == R.id.nav) {
+            return true;
+        } else if (id == R.id.web) {
+            return true;
+        } else if (id == R.id.others) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

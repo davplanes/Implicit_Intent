6e0d211bad73ffac76f41302944f1ebfa40b5410
user <davplanes@gmail.com>
2025-01-11 00:44:53 +0200

    Added icons and image buttons to display them

--- app/src/main/java/com/davidkerman/implicitintent/MainActivity.java
+++ app/src/main/java/com/davidkerman/implicitintent/MainActivity.java
@@ -2,6 +2,8 @@ package com.davidkerman.implicitintent;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private Toolbar     toolbar;
+    private ImageButton icon1;
+    private ImageButton icon2;
+    private ImageButton icon3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private void InitializeViews() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(findViewById(R.id.toolbar));
+
+        icon1 = findViewById(R.id.icon1);
+        icon2 = findViewById(R.id.icon2);
+        icon3 = findViewById(R.id.icon3);
     }
 
     @Override
@@ -45,26 +54,77 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.msg) {
             return true;
+        }
 
-        } else if (id == R.id.wa) {
+        else if (id == R.id.wa) {
+            icon1.setImageResource(R.drawable.wap);
+            icon2.setImageResource(R.drawable.wanp);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.INVISIBLE);
+
             return true;
+        }
 
-        } else if (id == R.id.othermsgs) {
+        else if (id == R.id.othermsgs) {
+            icon1.setImageResource(R.drawable.sms);
+            icon2.setImageResource(R.drawable.mail);
+            icon3.setImageResource(R.drawable.msg);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.VISIBLE);
+
             return true;
+        }
 
-        } else if (id == R.id.map) {
+        else if (id == R.id.map) {
             return true;
+        }
 
-        } else if (id == R.id.loc) {
+        else if (id == R.id.loc) {
+            icon1.setImageResource(R.drawable.mappcoords);
+            icon2.setImageResource(R.drawable.mappadrs);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.INVISIBLE);
+
             return true;
+        }
 
-        } else if (id == R.id.nav) {
+        else if (id == R.id.nav) {
+            icon1.setImageResource(R.drawable.gm);
+            icon2.setImageResource(R.drawable.wz);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.INVISIBLE);
+
             return true;
+        }
 
-        } else if (id == R.id.web) {
+        else if (id == R.id.web) {
+            icon1.setImageResource(R.drawable.web);
+            icon2.setImageResource(R.drawable.yt);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.INVISIBLE);
+
             return true;
+        }
 
-        } else if (id == R.id.others) {
+        else if (id == R.id.others) {
+            icon1.setImageResource(R.drawable.camera);
+            icon2.setImageResource(R.drawable.dial);
+            icon3.setImageResource(R.drawable.clck);
+
+            icon1.setVisibility(View.VISIBLE);
+            icon2.setVisibility(View.VISIBLE);
+            icon3.setVisibility(View.VISIBLE);
+
             return true;
         }
 
